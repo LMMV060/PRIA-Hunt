e2871a988fcea892a02c91fb3e70496d95f7454f
LMMV060 <lmmv060@gmail.com>
2026-01-10 20:22:25 +0100

    Añadido para que el modelo default sirva como espectador y pruebas con la camara en tercera persona

--- Proyecto completo/Assets/Scripts/Test/RaycastHiderTransformer.cs
+++ Proyecto completo/Assets/Scripts/Test/RaycastHiderTransformer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using Fusion;
+using TMPro;
 using UnityEngine;
 using Behaviour = Fusion.Behaviour;
 
@@ -9,6 +10,10 @@ public class RaycastHiderTransformer : NetworkBehaviour
     [SerializeField] private float rango = 20f;
     //[SerializeField] private LineRenderer lineRenderer;
     [SerializeField] private Transform modelRoot;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private LayerMask raycastMask;
+    [SerializeField] private GameObject propInfoPanel;
+    [SerializeField] private TextMeshProUGUI propNameText;
     
     void Update()
     {
@@ -20,37 +25,49 @@ public class RaycastHiderTransformer : NetworkBehaviour
 
     private void Transform()
     {
-        RaycastHit hit;
+        bool foundProp = false;
         Vector3 endPos = shootCam.position + shootCam.forward * rango;
+        RaycastHit[] hits = Physics.RaycastAll( shootCam.position,  shootCam.forward, rango, raycastMask);
 
-        if (Physics.Raycast(shootCam.position, shootCam.forward, out hit, rango))
+        foreach (RaycastHit hit in hits)
         {
-            endPos = hit.point;
-            
+            if (hit.collider.CompareTag("Hider"))
+                continue;
+
             Debug.Log($"Hit object: {hit.collider.gameObject.name} (Tag: {hit.collider.tag})");
+
+            foundProp = true;
+
+            // Mostrar UI
+            propInfoPanel.SetActive(true);
+            propNameText.text = hit.collider.gameObject.name;
             
-            if (Input.GetKeyDown(KeyCode.Mouse0))
+            if (Input.GetKeyDown(KeyCode.Mouse0) && hit.collider.CompareTag("Propt"))
             {
-                if (hit.collider.CompareTag("Propt"))
-                {
-                    Rpc_ReplaceModel(hit.collider.gameObject.name);
-                }
+                Rpc_ReplaceModel(hit.collider.gameObject.name);
             }
+
+            break; 
+        }
+        
+        if (!foundProp)
+        {
+            propInfoPanel.SetActive(false);
         }
 
         // Mostrar la línea en todos los clientes
         //Rpc_DrawShotLine(shootCam.position, endPos);
     }
    
-    /*
+    
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     private void Rpc_DrawShotLine(Vector3 start, Vector3 end)
     {
         StartCoroutine(DrawShotLine(start, end));
     }
-    */
+    
 
-    /*
+    
     private IEnumerator DrawShotLine(Vector3 start, Vector3 end)
     {
         lineRenderer.SetPosition(0, start);
@@ -62,7 +79,7 @@ public class RaycastHiderTransformer : NetworkBehaviour
 
         lineRenderer.enabled = false;
     }
-    */
+    
     
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     private void Rpc_ReplaceModel(string propName)
@@ -77,12 +94,21 @@ public class RaycastHiderTransformer : NetworkBehaviour
 
         GameObject newModel = Instantiate(prop, modelRoot);
         newModel.transform.localRotation = prop.transform.localRotation;
-        newModel.transform.localPosition = Vector3.zero; 
-        newModel.transform.localScale = prop.transform.localScale; 
+        newModel.transform.localScale = prop.transform.localScale;
+        newModel.transform.localPosition = new Vector3(0, prop.transform.position.y+1.5f, 0);
         newModel.tag = "Hider";
         
+        /*
+        Renderer rend = newModel.GetComponentInChildren<Renderer>();
+        if (rend != null)
+        {
+            float localMinY = rend.localBounds.min.y;
+            newModel.transform.localPosition = new Vector3(0, -localMinY, 0);
+        }
+        */
 
         // Eliminar todos los componentes que NO sean MeshRenderer, MeshFilter o Collider
+        
         foreach (var comp in newModel.GetComponentsInChildren<Component>())
         {
             if (comp is MeshRenderer || comp is MeshFilter || comp is Collider || comp is Transform)
